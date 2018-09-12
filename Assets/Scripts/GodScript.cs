086cd78fccc53f4963c812e606b30f7feaba454f
Alexandru Sandu <alexandrusandu@Alexandrus-MacBook-Pro.local>
2018-09-12 21:48:15 +0300

    god script created

---
+++ Assets/Scripts/GodScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GodScript : MonoBehaviour {
+
+    bool paused = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void pausUnpause(){
+		this.paused = !this.paused;
+	}
+}
